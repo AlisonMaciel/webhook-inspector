9e35f58f3465e345bb78252ce281539c5dc1a736
Alison <alisonbetini27@gmail.com>
2025-10-28 18:08:56 -0300

    creation of all functional parts of the front end

---
+++ web/src/components/ui/code-block.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState, type ComponentProps } from 'react';
+import { codeToHtml } from 'shiki';
+import { twMerge } from 'tailwind-merge';
+
+interface CodeBlockProps extends ComponentProps<'div'> {
+	code: string;
+	languague?: string;
+}
+
+export function CodeBlock({
+	className,
+	code,
+	languague = 'json',
+	...props
+}: CodeBlockProps) {
+	const [parsedCode, setParsedCode] = useState('');
+
+	useEffect(() => {
+		if (code) {
+			codeToHtml(code, { lang: languague, theme: 'vesper' }).then((parsed) =>
+				setParsedCode(parsed),
+			);
+		}
+	}, [code, languague]);
+	return (
+		<div
+			className={twMerge(
+				'relative rounded-lg border border-zinc-700 overflow-x-auto',
+				className,
+			)}
+			{...props}
+		>
+			<div
+				className="[&_pre]:p-4 [&_pre]:text-sm [&_pre]:font-mono [&_pre]:leading-relaxed"
+				dangerouslySetInnerHTML={{ __html: parsedCode }}
+			/>
+		</div>
+	);
+}
